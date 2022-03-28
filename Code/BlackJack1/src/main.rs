a2bdb268cfe3a4447aba2bf4c4754e2605545efd
Tanner C <tannerc3@illinois.edu>
2022-03-27 20:17:52 -0500

    3/27 Cards and Deck funcitonallity start

--- Code/BlackJack1/src/main.rs
+++ Code/BlackJack1/src/main.rs
@@ -1,3 +1,15 @@
+pub mod cards;
+use crate::cards::*;
+
 fn main() {
     println!("Hello, world!");
+    let test: Card = BuildCard("new");
+    println!("{}",test.name_);
+    let Hand1: Vec<Card> = BuildDeck();
+    let mut count = 0;
+    for card in Hand1 {
+        println!("{}: {}", card.name_, card.value_);
+        count += 1;
+    }
+    println!("Total Cards: {}", count);
 }
